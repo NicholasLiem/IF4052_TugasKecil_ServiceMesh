a08b1d71fe6aebac7a5344eb256fd07cc3de9152
nathania.calista01@gmail.com <nathania.calista01@gmail.com>
2024-11-25 22:01:41 +0700

    feat: client service

--- client-service/main.go
+++ client-service/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
     fmt.Println("Hello, World!")
+    app := gin.Default()
+    route := app.Group("/client-service")
+
+    route.POST("/appointments/:category", ReserveAppointment)
+    route.GET("/appointments/:patient_id", GetPatientAppointment)
+    route.GET("/records/:patient_id", GetPatientRecords)
+    route.POST("/records/:patient_id", UpdatePatientRecord)
+    
+    app.Run(":1233")
 }
